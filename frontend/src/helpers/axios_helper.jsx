ef1d5dd19c3cc382cf1038a77ca3ea8f6a48bef4
mehreen hossain chowdhury <mehreenhossain@iut-dhaka.edu>
2025-04-29 00:58:52 +0600

    endpoints changed

--- frontend/src/helpers/axios_helper.jsx
+++ frontend/src/helpers/axios_helper.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:8081';
+axios.defaults.baseURL = 'http://localhost:8080';
 
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 
